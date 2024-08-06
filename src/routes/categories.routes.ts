8e423017e27c8edca6e5cfab5da36662fa0fe127
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-06 23:20:52 +0100

    refatorando a api

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { createCategoryController } from '../modules/cars/useCases/CreateCategory/index';
+import { createCategoryController } from '../modules/cars/useCases/createCategory/index';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.status(201).json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
